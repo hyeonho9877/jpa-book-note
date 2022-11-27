3e8f5f1914fb40430d2bd3d759d4f57f9eaa08aa
hyeonho <6190606@naver.com>
2022-11-27 22:13:35 +0900

    Section.07 - demo-project

--- demo-project/src/main/java/domain/item/Item.java
+++ demo-project/src/main/java/domain/item/Item.java
@@ -1,11 +1,16 @@
-package domain;
+package domain.item;
+
+import domain.BaseEntity;
+import domain.Category;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Item extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "ITEM_ID")
